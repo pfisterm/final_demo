8b6b4f23bde0e2468b055bce6a0615f41993d280
pfisterm <pfisterm@users.noreply.github.com>
2022-04-25 18:16:46 -0400

    update outputs

--- app/app.py
+++ app/app.py
@@ -1,16 +1,27 @@
 
 
-data = {"number": "L21", "members": ["Catie", "Maddie", "Meher", "Nina", "Rebecca"], 
+data = {"members": ["Catie", "Maddie", "Meher", "Nina", "Rebecca"], 
 "chores": ["vaccuming", "kitchen counters", "stove", "kitchen and dining table", 
-"kitchen sink", "bathroom counter", "mirror", "showers", "toilets", "trash and recycling"] }
+"kitchen sink", "bathroom counter", "mirror", "showers", "toilets", "trash and recycling"]}
+
 
 import random
 
 chores = data["chores"]
 
+assignments = {}
+
 for member in data["members"]:
-    task_1 = ''.join(random.sample(chores, 1))
-    chores.remove(task_1)
-    task_2 = ''.join(random.sample(chores, 1))
-    chores.remove(task_2)
-    print(member, ":", task_1, ",", task_2)
+    assignments[member] = []
+
+while len(chores) > 0:
+    for member in data["members"]:
+        task = ''.join(random.choice(chores))
+        chores.remove(task)
+        assignments[member].append(task)
+
+for member in assignments:
+    email = input(f'Please enter {member}s email addresss:')
+    #assignments[member]["email"] = email
+
+print(assignments)
