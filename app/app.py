79a41c0a6743a4635e65cf6eed722c8c6138be56
pfisterm <pfisterm@users.noreply.github.com>
2022-04-27 17:18:17 -0400

    redo email entry

--- app/app.py
+++ app/app.py
@@ -12,7 +12,10 @@ userinput = input("Enter the name of the roommate:")
 
 while userinput != exit_loop:
     data["members"].append(userinput)
+    email = input(f'Please enter the email for {userinput}:')
+    data["emails"].append(email)
     userinput = input("Enter the name of the roommate:")
+    
 
 userinput = input("Enter the chore:")
 
@@ -20,22 +23,27 @@ while userinput != exit_loop:
     data["chores"].append(userinput) 
     userinput = input("Enter the chore:")
 
-userinput = input("Enter the roommate's email:")
-
-while userinput != exit_loop:
-    data["emails"].append(userinput)
-    userinput = input("Enter the roommate's email:")
 
 print(data)
 
 ## App Structure
 
-import(random)
+import random
 
-assignments{}
+assignments = dict()
 
 for member in data["members"]:
     assignments[member] = []
+
+#for member in assignments:
+   #assignments[member] = dict()
+    #member["email"] = []
+
+print(assignments)
+
+#for member in data["member"]:
+    #assignments[member]["email"].append(data["emails"])
+
 
 chores = data["chores"]
 
@@ -44,10 +52,6 @@ while len(chores) > 0:
         task = random.choice(chores)
         chores.remove(task)
         assignments[member].append(task)
-
-#for member in assignments:
-    #email = input(f'Please enter {member}s email addresss:')
-    #assignments[member]["email"] = email
 
 
 
