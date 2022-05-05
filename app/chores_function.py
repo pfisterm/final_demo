3c9ed7dd7637145d52d9dcfd8c73317f54665df3
pfisterm <pfisterm@users.noreply.github.com>
2022-05-05 11:23:53 -0400

    edit html

---
+++ app/chores_function.py
@@ -0,0 +1,43 @@
+#Adjusted chores function to run on web app
+
+
+def chores(members, chores, emails):
+
+    data = {"members": [], 
+    "chores": [], "emails":[]}
+
+    data["members"] = members
+    data["chores"] = chores
+    data["emails"] = emails
+
+
+    ## App Structure
+
+    import random
+
+    assignments = dict()
+
+    for member in data["members"]:
+        assignments[member] = []
+
+    for member in assignments:
+        assignments[member] = dict()
+        assignments[member]["tasks"] = []
+        email = input(f'Please enter the email for {member}:')
+        assignments[member]["email"] = email
+
+
+    chores = data["chores"]
+
+    while len(chores) > 0:
+        for member in assignments:
+            task = random.choice(chores)
+            chores.remove(task)
+            assignments[member]["tasks"].append(task)
+
+    for member in assignments:
+        tasks = assignments[member]["tasks"]
+        chores = ' & '.join(', '.join(tasks).rsplit(', ', 1))
+        print(f"{member}: {chores}")
+
+    return assignments
