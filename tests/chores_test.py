d32508d18f22b64098987aea8ab9c1213b67abdf
pfisterm <pfisterm@users.noreply.github.com>
2022-05-05 21:33:10 -0400

    pytest

---
+++ tests/chores_test.py
@@ -0,0 +1,6 @@
+import pytest
+
+from app.chores import assign_chores()
+
+#As our function does not take any parameters, and the user inputs information as the 
+    #function runs, we did not find it plauable or appropriate to run automated tests
